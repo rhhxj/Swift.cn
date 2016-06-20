115854bf6e22709c43655f4656266de65ef397f1
rhhxj <20141105047M@admindeiMac-08.local>
2016-06-20 08:18:48 +0800

    Initial Commit

---
+++ Swift.cn/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift.cn
+//
+//  Created by 20141105047 on 16/6/20.
+//  Copyright (c) 2016年 20141105047. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
